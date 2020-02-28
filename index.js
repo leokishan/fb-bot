64154c88287a78d845c92620393983a4b224113a
kishan <thakkarkishan097@gmail.com>
2020-02-28 21:46:35 +0530

    heroku config

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser')
 const handlers = require('./src/routes')
 
 const app = express()
+let port = process.env.PORT || 8082
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(handlers)
 
-app.listen(8082, () => console.log('Listening to 8082'))
+app.listen(port, () => console.log('Listening to 8082'))
 
 module.exports = app
